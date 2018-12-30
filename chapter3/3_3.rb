542a11e039c1e26fe492ad11b178431bbcb0274d
kakitaku00 <webtrakakinuma@gmail.com>
2018-12-30 23:29:40 +0900

    elsif

---
+++ chapter3/3_3.rb
@@ -0,0 +1,18 @@
+# 「そうでなければ」の処理、else
+wallet = 100
+if wallet >= 300
+  puts "コンビニでアイスを買って行こう!"
+else
+  puts "川沿いを散歩しよう"
+end
+# => 川沿いを散歩しよう
+
+season = "夏"
+if season == "春"
+  puts "アイスを買って行こう！"
+elsif == "夏"
+  puts "かき氷を買って行こう！"
+else
+  puts "あんまんを買って行こう"
+end
+# => かき氷を買って行こう！
