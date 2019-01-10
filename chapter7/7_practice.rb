2c30db66c2d6eb5dba79529ed8f434a7eb0c6639
kakitaku00 <webtrakakinuma@gmail.com>
2019-01-11 01:20:23 +0900

    7_practice

--- chapter7/7_practice.rb
+++ chapter7/7_practice.rb
@@ -33,3 +33,62 @@ def dice2
   end
 end
 dice2
+
+# 7-4
+def price(item, size = "ショート")
+  price = 0
+  if item == "コーヒー"
+    price += 300
+  end
+  if item == "カフェラテ"
+    price += 400
+  end
+  case size
+    when "ショート" then
+    return price
+    when "トール" then
+    return price += 50
+    when "ベンティ" then
+    return price += 100
+  end
+end
+puts price("コーヒー")
+puts price("カフェラテ")
+puts price("カフェラテ", "ベンティ")
+
+# 7-5
+def order(drink)
+  puts "#{drink}をください"
+end
+
+drink = "コーヒー"
+order(drink)
+
+# ======================
+
+# 問5模写
+def dice
+  result = [1, 2, 3, 4, 5, 6].sample
+  # 1以外なら結果を返す
+  return result unless result == 1
+  puts "もう一回"
+  [1, 2, 3, 4, 5, 6].sample
+end
+
+puts dice
+
+# 問6模写
+def price(item:)
+  items = { "コーヒー" => 300, "カフェラテ"  => 400}
+  items[item]
+end
+puts price(item: "コーヒー")
+puts price(item: "カフェラテ")
+
+# 問7模写
+def price(item: ,size: "ショート")
+  items = { "コーヒー" => 300, "カフェラテ"  => 400}
+  sizes = { "ショート" => 0, "トール" => 50, "ベンティ" => 100}
+  items[item] + sizes[size]
+end
+puts price(item: "コーヒー", size: "トール")
