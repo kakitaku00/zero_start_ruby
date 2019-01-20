4cb4b7ad07a1b9fa1bd81a3c6182db40dee57a16
kakitaku00 <webtrakakinuma@gmail.com>
2019-01-20 13:12:10 +0900

    gem bundle

--- chapter10/10_1.rb
+++ chapter10/10_1.rb
@@ -10,3 +10,13 @@ ap ["カフェラテ", "モカ", "コーヒー"]
 
 # Bundler
 # 複数のGemを簡単に管理するBundler
+# bundle initしGemfileを作成
+# Gemfileにgem "pry"と記述し、bundle install
+# gem install pryを実行したのと同じ処理がされる
+
+# Gemfile => Gemをインストールするための発注書
+# Gemfile.lock => 納品書
+
+# bundle updateコマンドでGemをバージョンアップする
+
+# bundle execコマンドで指定したバージョンのGemを使う
