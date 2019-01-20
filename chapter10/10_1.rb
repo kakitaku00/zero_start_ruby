139b663d34b42da0d8cd25abb18698a7210d8d60
kakitaku00 <webtrakakinuma@gmail.com>
2019-01-20 13:11:33 +0900

    gem bundle

---
+++ chapter10/10_1.rb
@@ -0,0 +1,12 @@
+# Gem
+# awesome_printというGemをインストールし、使ってみる
+require "awesome_print"
+ap ["カフェラテ", "モカ", "コーヒー"]
+# [
+#     [0] "カフェラテ",
+#     [1] "モカ",
+#     [2] "コーヒー"
+# ]
+
+# Bundler
+# 複数のGemを簡単に管理するBundler
